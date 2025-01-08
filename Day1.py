939841c242308379507d85145d5655e307f47f62
Gundu Vinay <gunduvinay07@gmail.com>
2025-01-08 09:27:07 +0530

    Day One code

---
+++ Day1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter integer:"))
+b=str(input("Enter string: "))
+c=float(input("Enetr float:"))
+d=True
+print("a",type(a))
+print("b",type(b))
+print("c",type(c))
+print("d",type(d))
